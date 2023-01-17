7a031e5abe077a1c8d5c6901cc7a72cf97df486e
Joel Oliveira <58565967+joelpoliveira@users.noreply.github.com>
2023-01-17 10:08:50 +0000

    minor changes to feature functions documentation

--- src/util/features.py
+++ src/util/features.py
@@ -1,30 +1,37 @@
 import librosa
 import numpy as np
 import scipy.stats as stats
-from scipy.spatial.distance import cosine
+from numpy.typing import ArrayLike
 
-def get_mean(signal : np.array) -> float:
+def get_mean(signal : ArrayLike) -> float:
+    """Return mean of digital signal"""
     return signal.mean()
 
-def get_std(signal : np.array) -> float:
+def get_std(signal : ArrayLike) -> float:
+    """Return standard deviation of digital signal"""
     return signal.std()
 
-def get_skewness(signal : np.array) -> np.array:
+def get_skewness(signal : ArrayLike) -> ArrayLike:
+    """Return skewness of digital signal"""
     return stats.skew(signal)
 
-def get_kurtosis(signal : np.array) -> np.array:
+def get_kurtosis(signal : ArrayLike) -> ArrayLike:
+    """Return kurtosis of digital signal"""
     return stats.kurtosis(signal)
 
-def get_median(signal: np.array) -> float:
+def get_median(signal: ArrayLike) -> float:
+    """Return median of digital signal"""
     return np.median(signal)
 
-def get_max(signal : np.array) -> float:
+def get_max(signal : ArrayLike) -> float:
+    """Return max. value of digital signal"""
     return signal.max()
 
-def get_min(signal : np.array) -> float:
+def get_min(signal : ArrayLike) -> float:
+    """Return min. value of digital signal"""
     return signal.min()
 
-def get_statistics(signal: np.array) -> tuple:
+def get_statistics(signal: ArrayLike) -> tuple:
     return (
         get_mean(signal),
         get_std(signal),
@@ -35,43 +42,49 @@ def get_statistics(signal: np.array) -> tuple:
         get_kurtosis(signal)
     )
 
-def extract_statistics(signal: np.array, axis=1):
+def extract_statistics(signal: ArrayLike, axis=1):
     return np.apply_along_axis(get_statistics, axis, signal).flatten()
 
-def mel_freq_cepstrum_coef(signal : np.array):
+def mel_freq_cepstrum_coef(signal : ArrayLike):
     return librosa.feature.mfcc(y=signal, n_mfcc=13)
 
-def spectral_centroid(signal : np.array):
+def spectral_centroid(signal : ArrayLike):
     return librosa.feature.spectral_centroid(y=signal)
 
-def spectral_bandwith(signal : np.array):
+def spectral_bandwith(signal : ArrayLike):
     return librosa.feature.spectral_bandwidth(y=signal)
 
-def spectral_contrast(signal : np.array):
+def spectral_contrast(signal : ArrayLike):
     return librosa.feature.spectral_contrast(y=signal)
 
-def spectral_flatness(signal : np.array):
+def spectral_flatness(signal : ArrayLike):
     return librosa.feature.spectral_flatness(y=signal)
 
-def spectral_rollof(signal : np.array):
+def spectral_rollof(signal : ArrayLike):
     return librosa.feature.spectral_rolloff(y=signal)
 
-def fundamental_frequency(signal : np.array):
+def fundamental_frequency(signal : ArrayLike):
     f0 = librosa.yin(signal, fmin=20, fmax=11025)
     f0[f0==11025] = 0
     return f0
 
-def root_mean_square(signal: np.array):
+def root_mean_square(signal: ArrayLike):
     return librosa.feature.rms(y=signal)
 
-def zero_crossing_rate(signal: np.array):
+def zero_crossing_rate(signal: ArrayLike):
     return librosa.feature.zero_crossing_rate(y=signal)
 
-def get_tempo(signal: np.array):
+def get_tempo(signal: ArrayLike):
     return librosa.beat.tempo(y=signal)
 
-def get_feature_vector(signal: np.array) -> np.array:
-    if signal.dtype==np.int16: # or any type of int, in reality
+def get_feature_vector(signal: ArrayLike) -> ArrayLike:
+    if len(signal.shape)>1: #must be single channel
+        if signal.shape[1]==1:
+            signal = signal.flatten()
+        else:
+            signal = signal.mean(axis=1)
+
+    if np.issubdtype(signal.dtype, np.integer): # or any type of int, in reality
         signal = librosa.util.buf_to_float(signal)
     
     mfcc = mel_freq_cepstrum_coef(signal)
