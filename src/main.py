cabcca8c86a96132ddebc4a8ff1dfc202aef8808
Joel Oliveira <58565967+joelpoliveira@users.noreply.github.com>
2023-01-17 10:10:45 +0000

    minor changes in main

--- src/main.py
+++ src/main.py
@@ -1,21 +1,22 @@
-import sounddevice as sd
+import os
+
 from scipy.io import wavfile
 from midi2audio import FluidSynth
 from midiutil import MIDIFile
 from io import BytesIO
 from util.features import get_feature_vector
-import os
 
-
-print(os.getcwd())
-FONT = "guitar.sf2"
+FONT = "piano_chords.sf2"
+MIDI_FILE = "major-scale.midi"
+WAV_FILE = "major-scale.wav"
 
 degrees  = [60, 62, 64, 65, 67, 69, 71, 72] # MIDI note number
+degrees = [4, 6, 8, 9, 11, 13, 15, 16]
 track    = 0
 channel  = 0
 time     = 0   # In beats
 duration = 1   # In beats
-tempo    = 60  # In BPM
+tempo    = 100  # In BPM
 volume   = 100 # 0-127, as per the MIDI standard
 
 
@@ -27,19 +28,24 @@ for pitch in degrees:
     MyMIDI.addNote(track, channel, pitch, time, duration, volume)
     time = time + 1
 
+with open(f"./input/{MIDI_FILE}", "wb") as output_file:
+    MyMIDI.writeFile(output_file)
+
 song = BytesIO()
 wav_song = BytesIO( )
 
 MyMIDI.writeFile(song)
 FluidSynth(
         sound_font=f"./soundfonts/{FONT}"
-    ).midi_to_audio("./major-scale.mid", "./major-scale.wav")
+    ).midi_to_audio(f"./input/{MIDI_FILE}", f"./output/{WAV_FILE}")
 
-fs, song = wavfile.read("./major-scale.wav")
+fs, song = wavfile.read(f"./output/{WAV_FILE}")
 #print(song)
 #sd.play(song, fs)
 #sd.wait()
 # sd.play()
-print(get_feature_vector(song))
-# with open("major-scale.mid", "wb") as output_file:
-#     MyMIDI.writeFile(output_file)
+#print(get_feature_vector(song))
+
+
+if __name__=="__main__":
+    pass
